1aab382fbebd0bcc294fd3e9f48532242819c052
Kai Brandes <kai.brandes@otto.de>
2016-05-25 21:47:22 +0200

    renaming

--- src/finalytics/csv/csv_loader.clj
+++ src/finalytics/csv/csv_loader.clj
@@ -6,11 +6,11 @@
 
 (defn load-csv-data []
   (log/info "-> loading csv-data")
-  (let [data-spec (pars/load-data-spec "resources/public/data/spec.edn")
+  (let [col-spec (pars/load-data-spec "resources/public/data/col-spec.edn")
         tid-spec (pars/load-tid-spec "resources/public/data/tid-spec.edn")
         class-spec (pars/load-class-spec "resources/public/data/class-spec.edn")]
     (-> (pars/load-csv "resources/public/data/csv")
-        (pars/with-columns data-spec)
+        (pars/with-columns col-spec)
         (pars/with-tids tid-spec :client)
         (pars/with-classification class-spec))))
 
