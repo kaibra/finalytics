3688e1608311715ca8c0452928f5fc908e006faa
Kai Brandes <kai.brandes@otto.de>
2016-07-09 11:29:24 +0200

    a fix for graphs being larger then svg-viewport

--- src-cljs/finalytics/charts/waterfallchart.cljs
+++ src-cljs/finalytics/charts/waterfallchart.cljs
@@ -17,7 +17,7 @@
                                   :pos             pos
                                   :transaction     transaction}))))))
 
-(defn waterfall-chart-conf [all-transactions]
+(defn waterfall-chart-conf [all-transactions last-month-result]
   (let [waterfall-range (waterfall-range all-transactions)
         waterfall-value-range (map :waterfall-value waterfall-range)
         drawing-height 500
@@ -27,8 +27,8 @@
      :bar-width       bar-width
      :chart-width     (* bar-width (count all-transactions))
      :chart-height    (+ drawing-height (* 2 top-bottom-space))
-     :yscale          (utils/d3YScale (Math/max (Math/abs (apply min waterfall-value-range))
-                                                (Math/abs (apply max waterfall-value-range)))
+     :yscale          (utils/d3YScale (Math/max (Math/abs (+ (apply min waterfall-value-range) last-month-result))
+                                                (Math/abs (+ (apply max waterfall-value-range) last-month-result)))
                                       drawing-height)}))
 
 (defn append-x-axis [svg-container chart-height chart-width]
@@ -41,30 +41,32 @@
 (defn render-waterfall [svg-container last-month-result {:keys [waterfall-range bar-width chart-height chart-width yscale]}]
   (append-x-axis svg-container chart-height chart-width)
   (loop [the-range waterfall-range
-         last-axis-pos (or last-month-result (/ chart-height 2))]
+         last-axis-pos (or last-month-result 0)]
     (if (empty? the-range)
       last-axis-pos
       (let [{:keys [pos transaction]} (first the-range)
             {:keys [columns color]} transaction
-            transaction-value (yscale (get-in transaction [:columns :value]))
+            transaction-value (get-in transaction [:columns :value])
+            transaction-value-scaled (yscale transaction-value)
             the-color (or color "rgb(0,0,255)")
-            y (if (< transaction-value 0)
-                last-axis-pos
-                (- last-axis-pos transaction-value))]
+            last-axis-pos-scaled (- (/ chart-height 2) (yscale last-axis-pos))
+            y (if (< transaction-value-scaled 0)
+                last-axis-pos-scaled
+                (- last-axis-pos-scaled transaction-value-scaled))]
         (-> (.append svg-container "rect")
             (.attr "x" (* pos bar-width))
             (.attr "y" y)
             (.attr "style" (str "fill: " the-color ";stroke-width:1;stroke:rgb(0,0,0)"))
             (.attr "data-val" columns)
             (.attr "width" bar-width)
-            (.attr "height" (Math/abs transaction-value)))
-        (recur (rest the-range) (- last-axis-pos transaction-value))))))
+            (.attr "height" (Math/abs transaction-value-scaled)))
+        (recur (rest the-range) (+ last-axis-pos transaction-value))))))
 
 (defn waterfall-chart [container csv-data _]
   (utils/render-chart
     container csv-data
     (fn [last-month-result c {:keys [days]}]
       (let [all-transactions (flatten (vals days))
-            cconf (waterfall-chart-conf all-transactions)
+            cconf (waterfall-chart-conf all-transactions last-month-result)
             svg-container (utils/append-svg-container c cconf)]
         (render-waterfall svg-container last-month-result cconf)))))
